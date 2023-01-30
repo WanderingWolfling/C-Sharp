02facc744592833111e8c5ecab8727d4de3ff898
Wolfy <vaencomat@gmail.com>
2023-01-31 00:38:24 +0200

    Finish second task. Its such as first task

---
+++ Seminar_1/Dz_2/Dz_2.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+int.TryParse(Console.ReadLine(), out int a);
+int.TryParse(Console.ReadLine(), out int b);
+int.TryParse(Console.ReadLine(), out int c);
+
+var max = 0;
+
+if (a > b & a > c)
+{
+    max = a;
+}
+else if (b > a & b > c)
+{
+    max = b;
+}
+
+else
+{
+
+    max = c;
+}
+Console.WriteLine(max);
