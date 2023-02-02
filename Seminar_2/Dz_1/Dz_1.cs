586e40902b21ccb9a6d28dbf363918623fe198c2
Wolfy <vaencomat@gmail.com>
2023-02-02 23:48:20 +0200

    End fits task. Use internet to fing method

--- Seminar_2/Dz_1/Dz_1.cs
+++ Seminar_2/Dz_1/Dz_1.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трёхзначное число:");
+int.TryParse(Console.ReadLine(), out int a);
+string text = a.ToString();
+
+if (text.Length > 3)
+{
+    Console.WriteLine($"Введи трёхзначное число, братан, моя твоя не понимать. {text} - не трёхзначное число");
+
+}
+else
+{
+    int find = 2;
+    char found_text = text[find];
+    Console.WriteLine(found_text);
+}
+
+
+
+// Console.WriteLine($"{text}");
