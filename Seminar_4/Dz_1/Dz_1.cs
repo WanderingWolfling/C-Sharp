c3e9ea4c23e971904777688e9ec97a0e608227c0
Wolfy <vaencomat@gmail.com>
2023-02-09 23:48:53 +0200

    Its very simple

--- Seminar_4/Dz_1/Dz_1.cs
+++ Seminar_4/Dz_1/Dz_1.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+System.Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out int a);
+
+System.Console.WriteLine("Введите степень");
+int.TryParse(Console.ReadLine(), out int b);
+
+System.Console.WriteLine(Math.Pow(a, b));
+
+
+
+// while (b <= a)
+// {
+//     System.Console.WriteLine(Math.Pow(b, 3));
+//     b++;
+// }
