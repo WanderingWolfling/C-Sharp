04167496e224a6c8301b79eed30f5475c276f565
Wolfy <vaencomat@gmail.com>
2023-02-11 15:43:48 +0200

    Start undestend functions

---
+++ Seminar_4/Dz_2/Dz_2.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out int number);
+Console.WriteLine(CalcNumbs(number));
+
+
+int CalcNumbs(int number)
+{
+    int calc = 0;
+    while (number > 0)
+    {
+        calc += number % 10;
+        number = number / 10;
+    }
+
+    return calc;
+}
+
+
+
+// int CalculateNumbers(int number)
+// {
+//   int count = 0;
+
+//   while (number > 0) 
+//   {
+//     number /= 10;
+//     count++;
+//   }
+
+//   return count;
+// }
