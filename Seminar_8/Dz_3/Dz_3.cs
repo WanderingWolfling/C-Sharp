e4735b6e1a99fb52e020c6ae8d4aaf2125821d0b
Wolfy <vaencomat@gmail.com>
2023-02-24 13:52:13 +0200

    Write simple code, trying to do function

--- Seminar_8/Dz_3/Dz_3.cs
+++ Seminar_8/Dz_3/Dz_3.cs
@@ -53,6 +53,38 @@ string TwoDimensionArrayToString(int[,] array)
     return result;
 }
 
+
+if (matrix1.GetLength(1) != matrix2.GetLength(0))
+{
+    Console.WriteLine("Невозможно выполнить умножение матриц");
+}
+else
+{
+    int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+
+    Console.WriteLine("Результат умножения двух матриц:");
+
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            Console.Write(resultMatrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
 int[,] array_1 = CreateTwoDimensionArray(ReadInt("First Length"), ReadInt("Second Length"));
 Console.WriteLine("U create Array!");
 int[,] array_2 = CreateTwoDimensionArray(ReadInt("First Length"), ReadInt("Second Length"));
