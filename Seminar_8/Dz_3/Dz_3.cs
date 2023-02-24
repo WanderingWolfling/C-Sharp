7eb4293accb59fe8d6f973ef93d9a6abfaceaf29
Wolfy <vaencomat@gmail.com>
2023-02-24 13:30:05 +0200

    Copy old code from lessons

--- Seminar_8/Dz_3/Dz_3.cs
+++ Seminar_8/Dz_3/Dz_3.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+int ReadInt(string arg)
+{
+    Console.Write($"Input {arg}: ");
+    int result = 0;
+
+    while (!int.TryParse(Console.ReadLine(), out result))
+    {
+        Console.Write("Try again! ");
+    }
+    return result;
+}
+
+int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+    int[,] result = new int[firstLength, secondLength];
+    Random rand = new Random();
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = rand.Next(0, 10);
+        }
+    }
+
+    return result;
+}
+
+
+string TwoDimensionArrayToString(int[,] array)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result += $"{array[i, j]} ";
+        }
+
+        result += Environment.NewLine;
+    }
+
+    return result;
+}
+
+int[,] array_1 = CreateTwoDimensionArray(ReadInt("First Length"), ReadInt("Second Length"));
+Console.WriteLine("U create Array!");
+int[,] array_2 = CreateTwoDimensionArray(ReadInt("First Length"), ReadInt("Second Length"));
+Console.WriteLine("U create Array!");
+Console.WriteLine(TwoDimensionArrayToString(array_1));
+Console.WriteLine(TwoDimensionArrayToString(array_2));
