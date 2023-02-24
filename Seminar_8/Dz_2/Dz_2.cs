1f7d367a08e46056466e6b347a50cd4a91da5278
Wolfy <vaencomat@gmail.com>
2023-02-24 13:21:56 +0200

    We can use a lot of our lessons. Its task not so hard

--- Seminar_8/Dz_2/Dz_2.cs
+++ Seminar_8/Dz_2/Dz_2.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int ReadInt(string arg)
+{
+    Console.Write($"Input {arg}: ");
+    int result = 0;
+
+    while (!int.TryParse(Console.ReadLine(), out result))
+    {
+        Console.Write("Try again! ");
+    }
+
+    return result;
+}
+
+int[,] CreateTwoDimensionArray(int firstLength, int secondLength)
+{
+    int[,] result = new int[firstLength, secondLength];
+    Random rand = new Random();
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = rand.Next(0, 10);
+        }
+    }
+
+    return result;
+}
+
+
+string TwoDimensionArrayToString(int[,] array)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result += $"{array[i, j]} ";
+        }
+
+        result += Environment.NewLine;
+    }
+
+    return result;
+}
+
+int[,] array = CreateTwoDimensionArray(ReadInt("First Length"), ReadInt("Second Length"));
+Console.WriteLine(TwoDimensionArrayToString(array));
+//Взято с прошлых дз
+
+int FindLessRow(int[,] array)
+{
+    int minRowIndex = 0;
+    int minRowSum = int.MaxValue;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int rowSum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            rowSum += array[i, j];
+        }
+        if (rowSum < minRowSum)
+        {
+            minRowIndex = i;
+            minRowSum = rowSum;
+        }
+    }
+    return minRowIndex;
+}
+
+Console.WriteLine($"Строка с наименьшей суммой элементов: {FindLessRow(array) + 1}-я строка");
